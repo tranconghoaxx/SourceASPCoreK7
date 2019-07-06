d440945943c952615b8abad36d2ed711f5abfbbc
Hien Luong <hyhien@gmail.com>
2019-07-06 21:22:12 +0700

    D04 - jQuery

---
+++ D04_jQuery/D04_jQuery/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using D04_jQuery.Models;
+
+namespace D04_jQuery.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ D04_jQuery/D04_jQuery/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace D04_jQuery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
