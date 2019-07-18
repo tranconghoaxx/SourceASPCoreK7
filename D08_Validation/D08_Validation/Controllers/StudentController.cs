04e362b905a7045d726faa09cdfe866f264f18e7
Hien Luong <hyhien@gmail.com>
2019-07-18 21:24:50 +0700

    D08 - Validation

---
+++ D08_Validation/D08_Validation/Controllers/StudentController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using D08_Validation.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace D08_Validation.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            //StudentInfo sv = new StudentInfo();
+            //return View(sv);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(StudentInfo sv, string SaveFile)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("loi", "Không hợp lệ trên server");
+                return View();
+            }
+
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+
+            if (SaveFile == "Save Text file")
+            {
+                string[] lines = {
+                    sv.StudentId.ToString(),
+                    sv.FullName,
+                    sv.Mark.ToString(),
+                    sv.IsPass.ToString(),
+                    sv.Rank
+                };
+
+                System.IO.File.WriteAllLines(
+                    Path.Combine(path, "Student.txt"), lines);
+            }
+            else if (SaveFile == "Save JSON file")
+            {
+                System.IO.File.WriteAllText(
+                    Path.Combine(path, "Student.json"),
+                    JsonConvert.SerializeObject(sv)
+                );
+            }
+
+            return View(sv);
+        }
+
+        public IActionResult ReadFile(string type)
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+            StudentInfo sv = new StudentInfo();
+
+            if(type == "json")
+            {
+                string content = System.IO.File.ReadAllText(Path.Combine(path, "Student.json"));
+
+                sv = JsonConvert.DeserializeObject<StudentInfo>(content);
+            }
+            else if(type == "text")
+            {
+                string[] content = System.IO.File.ReadAllLines(Path.Combine(path, "Student.txt"));
+
+                sv.StudentId = int.Parse(content[0]);
+                sv.FullName = content[1];
+                sv.Mark = float.Parse(content[2]);
+                //sv.Mark = Convert.ToSingle(content[2]);
+            }
+
+            return View("Index", sv);
+        }
+    }
+}
---
+++ D08_Validation/D08_Validation/Models/StudentInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D08_Validation.Models
+{
+    public class StudentInfo
+    {
+        [Display(Name ="Mã sinh viên")]
+        public int StudentId { get; set; }
+        [Display(Name ="Họ tên")]
+        [Required(ErrorMessage ="*")]
+        [MaxLength(50, ErrorMessage ="Tối đa 50 kí tự")]
+        public string FullName { get; set; }
+        [Display(Name ="Điểm")]
+        [Range(0, 10, ErrorMessage ="Từ 0 --> 10")]
+        public float Mark { get; set; }
+        [Display(Name = "Đậu")]
+        public bool IsPass => Mark >= 5.5;
+        [Display(Name ="Xếp loại")]
+        public string Rank
+        {
+            get
+            {
+                string result = string.Empty;
+                if (Mark < 5.5)  result = "Yếu";
+                else if (Mark < 7.8) result = "Trung bình";
+                else if (Mark < 8.5) result = "Khá";
+                else if (Mark < 9) result = "Giỏi";
+                else result = "Xuất sắc";
+
+                return result;
+            }
+        }
+    }
+}
