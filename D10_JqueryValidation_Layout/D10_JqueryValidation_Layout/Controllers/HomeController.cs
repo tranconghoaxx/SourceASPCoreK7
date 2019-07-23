b08012d18da467f4c20247fc909f6f012368fb50
Hien Luong <hyhien@gmail.com>
2019-07-23 18:57:30 +0700

    D10 - Create project and layout Register page

---
+++ D10_JqueryValidation_Layout/D10_JqueryValidation_Layout/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using D10_JqueryValidation_Layout.Models;
+
+namespace D10_JqueryValidation_Layout.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
